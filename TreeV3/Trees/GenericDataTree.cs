2972087c7e944c708cd5f1f0425b60c5c3c6fafd
timo.toivonen <timo.toivonen@gfs-web.de>
2017-08-29 15:13:49 +0200

    Added comments.

--- TreeV3/Trees/GenericDataTree.cs
+++ TreeV3/Trees/GenericDataTree.cs
@@ -3,6 +3,9 @@ using TreeV3.Nodes;
 
 namespace TreeV3.Trees
 {
+    /// <summary>
+    /// A generic tree. All nodes, including root are a pre-defined string-keyed rootnodes, whose payload can vary.
+    /// </summary>
     public class GenericDataTree
     {
         public GenericDataTree(TreeNodeWithStringKey root)
@@ -13,6 +16,9 @@ namespace TreeV3.Trees
         public TreeNodeWithStringKey Root { get; private set; }
     }
 
+    /// <summary>
+    /// A string-keyed node. Payload can vary.
+    /// </summary>
     public class TreeNodeWithStringKey : TreeNode<string>
     {
         public TreeNodeWithStringKey(string id, object payload, IChildFinder<string> finder = null) : base(id, payload, finder)
